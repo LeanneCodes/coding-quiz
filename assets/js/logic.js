cf8ad76d937a9822fb35e9045cdbe1d224dd9a3f
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-04 14:44:01 +0000

    display if the event target chosen is correct or not

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,7 +4,7 @@
     2. each question contains buttons for each answer
     3. when an answer is clicked, text on the html page displays whether that answer was right or wrong
     4. if the answer is wrong, 10 seconds is deducted from the timer
-    5. the wuiz should end either when the timer reaches 0 or all questions are answered
+    5. the quiz should end either when the timer reaches 0 or all questions are answered
     6. when the game ends, it should display the user's score and give the user the ability to save their initials and their score
     7. the user can leave the game and come back and view their previous highscores
 */
@@ -16,9 +16,10 @@ var timer = document.getElementById('time');
 var questionDiv = document.getElementById('questions');
 var question = document.getElementById('question-title');
 var choices = document.getElementById('choices');
+var optionBtns = document.querySelectorAll(".optBtn");
 
 // 1. a start button that when clicked, a timer starts and the first question appears
-var timeLeft = 120;
+var timeLeft = 1200;
 
 startBtn.addEventListener('click', function() {
     startScreen.textContent = '';
@@ -39,26 +40,56 @@ startBtn.addEventListener('click', function() {
         }
     }, 1000);
 
-    showQuestion();
+    showQuestionAndOptions();
 
 });
 
-// 2. each question contains buttons for each answer
-function showQuestion() {
+// 2. each question contains buttons for each answer and when an option is clicked, the correct verdict is shown
+function showQuestionAndOptions() {
     questionDiv.classList.remove("hide");
-    var optionsList = document.createElement("ul");
-    choices.append(optionsList);
     
     for (var i = 0; i < multipleChoice.length; i++) {
         question.textContent = multipleChoice[i].question;
         var answerOptions = Object.values(multipleChoice[i].options);
+        var answer = multipleChoice[i].answer;
 
         for (var j = 0; j < answerOptions.length; j++) {
             console.log(answerOptions[j]);
             var option = answerOptions[j];
             var optionBtn = document.createElement("button");
             optionBtn.textContent = option;
-            optionsList.append(optionBtn);
+            optionBtn.classList.add("optBtn");
+            choices.append(optionBtn);
         }
+
+        console.log(answerOptions);
+        console.log(answer);
+
+        choices.addEventListener('click', function(event) {
+            console.log(event);
+            console.log(event.target.textContent);
+            if (event.target.textContent === answer) {
+                console.log("Correct!");
+                showCorrectVerdict();
+            } else {
+                console.log("Wrong!");
+                showIncorrectVerdict();
+            }
+        })
     }
+}
+
+// 3. when an answer is clicked, text on the html page displays whether that answer was right or wrong
+function showCorrectVerdict() {
+    var paraEl = document.createElement("p");
+    questionDiv.append(paraEl);
+    paraEl.textContent = "Correct!";
+    paraEl.setAttribute("style", "color: grey; font-style: italic; border-top: 1px solid grey; padding-top: 5px;");
+}
+
+function showIncorrectVerdict() {
+    var paraEl = document.createElement("p");
+    questionDiv.append(paraEl);
+    paraEl.textContent = "Wrong!";
+    paraEl.setAttribute("style", "color: grey; font-style: italic; border-top: 1px solid grey; padding-top: 5px;");
 }
