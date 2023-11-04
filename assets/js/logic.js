1fbf7183519f09a75087859aafd620f9ecefebe2
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-04 10:35:37 +0000

    add logic for the game

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+/*
+    Logic for the quiz
+    1. a start button that when clicked, a timer starts and the first question appears
+    2. each question contains buttons for each answer
+    3. when an answer is clicked, text on the html page displays whether that answer was right or wrong
+    4. if the answer is wrong, 10 seconds is deducted from the timer
+    5. the wuiz should end either when the timer reaches 0 or all questions are answered
+    6. when the game ends, it should display the user's score and give the user the ability to save their initials and their score
+    7. the user can leave the game and come back and view their highscores without needing to play the game
+*/
