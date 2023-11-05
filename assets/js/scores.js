992c2744101e9100d90e1adc5ae56486c405c00e
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-05 00:18:56 +0000

    testing the for loop works with the descending scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -10,6 +10,16 @@ function showHighscores() {
     highscoresArray.sort(function(a, b) {
         return b.remainingSeconds - a.remainingSeconds;
     });
+
+    highscores.innerHTML = "";
+
+    for (var i = 0; i < highscoresArray.length; i++) {
+        var score = highscoresArray[i];
+        var scoreLi = document.createElement("li");
+        scoreLi.textContent = score.initials + " - " + score.remainingSeconds;
+        highscores.appendChild(scoreLi);
+    }
+
 }
 
 showHighscores();
